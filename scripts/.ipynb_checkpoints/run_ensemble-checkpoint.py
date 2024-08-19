b44730499933ba536ea2b6f487dd537695a23d3a
aktriplett <aktriplett@email.arizona.edu>
2024-08-19 13:18:02 -0400

    changing how static inputs are copied

--- scripts/.ipynb_checkpoints/run_ensemble-checkpoint.py
+++ scripts/.ipynb_checkpoints/run_ensemble-checkpoint.py
@@ -8,34 +8,20 @@ from parflow.tools.settings import set_working_directory
 from pathlib import Path
 from glob import glob
 from parflow.tools.io import read_pfb, write_pfb, read_clm, read_pfb_sequence
-from pf_ens_functions import setup_baseline_run, calculate_water_table_depth, calculate_flow
+from pf_ens_functions import calculate_water_table_depth, calculate_flow
 from datetime import datetime, timedelta
 import xarray as xr
 import numpy as np
 import pandas as pd
 import parflow as pf
-model_eval_path = os.path.abspath('/home/at8471/c2_sbi_experiments/model_evaluation')
-sys.path.append(model_eval_path)
-from model_evaluation import get_observations, get_parflow_output, calculate_metrics, explore_available_observations, get_parflow_output_nc
-from plots import plot_obs_locations, plot_time_series, plot_compare_scatter, plot_metric_map
 
-base_dir = sys.argv[1]
+out_dir = sys.argv[1]
 runname = sys.argv[2]
-huc = sys.argv[3]
+i = sys.argv[3]
 num_hours = float(sys.argv[4])
 start = sys.argv[5]
-end = sys.argv[6]
-P = int(sys.argv[7])
-Q = int(sys.argv[8])
-
-grid = "conus2"
-temporal_resolution = "daily"
-variable_list = ["streamflow"]
-
-setup_baseline_run(base_dir = base_dir, runname = runname, hucs = [huc], start=start, end = end, P=P, Q=Q, hours = num_hours)
-
-out_dir = f"{base_dir}/outputs/{runname}"
-set_working_directory(out_dir)
+      
+set_working_directory(f'{out_dir}')
 
 run = Run.from_definition(f'{out_dir}/{runname}.yaml')
 run.TimingInfo.StopTime = num_hours
@@ -48,7 +34,7 @@ slope_x = pf.read_pfb(slope_x_file)
 
 slope_y_file = f'{out_dir}/slope_y.pfb'
 slope_y = pf.read_pfb(slope_y_file)
-mannings = pf.read_pfb(f'{out_dir}/mannings.pfb')
+mannings = pf.read_pfb(f'{out_dir}/mannings_{i}.pfb')
 
 dz = data.dz
 dx = 1000.0
@@ -89,7 +75,7 @@ ds['et'] = clm.isel(feature=et_idx)
 ds = ds.resample(time='1D').mean()
 
 ds['mannings'] = xr.DataArray(
-    read_pfb(f'{out_dir}/mannings.pfb')[0,:,:],
+    read_pfb(f'{out_dir}/mannings_{i}.pfb')[0,:,:],
     dims=('y','x')
 )
 ds['porosity'] = xr.DataArray(
@@ -117,7 +103,7 @@ ds = ds.assign_coords({
 
 ds = ds.astype(np.float32)
 
-ds.to_netcdf(f'{out_dir}/{runname}.nc', mode='w')
+ds.to_netcdf(f'{out_dir}/{runname}_{i}.nc', mode='w')
 
 # Clean up
 del ds
@@ -128,32 +114,3 @@ _ = [os.remove(os.path.abspath(f)) for f in saturation_files]
 _ = [os.remove(os.path.abspath(f)+'.dist') for f in saturation_files]
 _ = [os.remove(os.path.abspath(f)) for f in clm_files]
 _ = [os.remove(os.path.abspath(f)+'.dist') for f in clm_files]
-
-#evaluate baseline at target locations 
-ij_bounds, mask = subsettools.define_huc_domain([huc], grid)
-
-for variable in variable_list:
-    # Get observation data for sites in domain
-    obs_metadata_df, obs_data_df = get_observations(mask, ij_bounds, grid, start, end,
-                                                    variable, temporal_resolution)
-    print("created obsv df")
-    
-    obs_metadata_df.to_csv(f"{out_dir}/{variable}_{temporal_resolution}_metadf.csv", index=False)
-    obs_data_df.to_csv(f"{out_dir}/{variable}_{temporal_resolution}_df.csv", index=False)
-
-    # Get ParFlow outputs matching site locations
-    parflow_data_df = get_parflow_output_nc(f"{out_dir}/{runname}.nc", f'{out_dir}/{variable}_{temporal_resolution}_metadf.csv',var_name = variable, write_path = f"{out_dir}/{variable}_{temporal_resolution}_pfsim.csv")
-
-    print("created pf df")
-    
-    # Calculate metrics comparing ParFlow vs. observations
-    obs_data_df = obs_data_df.dropna(axis=1)
-    common_columns = obs_data_df.columns.intersection(parflow_data_df.columns)
-    obs_data_df = obs_data_df[common_columns]
-    parflow_data_df = parflow_data_df[common_columns]
-    obs_metadata_df= obs_metadata_df[obs_metadata_df['site_id'].isin(common_columns)]
-    
-    metrics_df = calculate_metrics(obs_data_df, parflow_data_df, obs_metadata_df,
-                                   write_csv=True, csv_path=f"{out_dir}/{variable}_metrics.csv")
-    print("calculated metrics")
-    
---
+++ scripts/.ipynb_checkpoints/setup_run_eval_baseline-checkpoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=mann_bl
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=16      # total number of tasks per node
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=01:00:00
+
+module load parflow-shared
+
+base_dir='/home/at8471/c2_sbi_experiments/sbi_framework'
+runname='sinnemahoning'
+huc='02050202'
+hours=2208
+
+start="2002-10-01"
+end="2002-12-31"
+
+P=4
+Q=4
+
+# Set up and do baseline run
+python3 setup_run_eval_baseline.py "$base_dir" "$runname" "$huc" "$hours" "$start" "$end" "$P" "$Q"
+
